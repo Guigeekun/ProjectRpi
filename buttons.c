49ae097f10e7dfdabd15332d552b5b8398ca35a3
Guigeek <geegeek21@gmail.com>
2020-04-07 15:51:30 +0200

    octSwap now allows 3 separate oct (3 - 4 - 5)

--- buttons.c
+++ buttons.c
@@ -11,10 +11,17 @@ fonction de portaudio pour émettre un son*/
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define Harm 10
+
 //on utilisera les 16 bouttons de la malette joyPI comme touche pour les sons
 //et les 4 boutons à part pour des commandes spéciales (enregister une composition, etc)
 
 //scanButton test la valeur envoyé par le l'input sélectionné
+int oct=4;
+
+int Freq[12] ={262,277,293,311,330,349,370,392,415,440,466,493};
+//les freq correspondent à l'octave 4 (définie vulgairement comme le centre du piano) voici les notes dans l'ordre :
+//Do4 (C4), Do#4 (C#4), Ré4 (D4), Ré#4 (D#4), Mi4 (E4), Fa4 (F4), Fa#4 (F#4), Sol4 (G4), Sol#4 (G#4), La4 (A4), La#4 (A#4), Si4 (B4)
 
 void scanButton (int boutton) 
 {
@@ -35,8 +42,10 @@ void fonction(int boutton)
 void tune(int freq){ //make sure that sox is installed before running
 //the way this function works is that it create a string which contains the sox command and then use it via the system() call
 
-    int Harm=10; // number of harmonics to play, feel free to mess with that
+  
     pid_t  pid;
+    if(oct==5){freq=freq*2;}
+    if(oct==3){freq=freq*0.5;}
 
     int volu=-10; // change that at your own risk, may harm your speaker and/or your hears
 
@@ -56,6 +65,7 @@ void tune(int freq){ //make sure that sox is installed before running
         pid=fork();
         if(pid==0){ //to play all the harmonics at the same time we need to do some kind of multi threading
             system(command);
+            sleep(1);
             exit(-1);
         }
         freq=freq*2; //maybe we can get some special sounds by messing with that
@@ -63,10 +73,27 @@ void tune(int freq){ //make sure that sox is installed before running
     }
     
     for(int i;i<Harm;i++){
+        
         wait(0);
     }
 
 }
+
+void octSwap(int flag){ //le flag 0 indique que l'on veut descendre d'un octave / le flag 1 indique que l'on veut monter d'un octave
+  //WARNING techniquement on peut utiliser cette commande une infinité de fois pour avoir des fréquence tres basses ou tres haute ... ne faites pas ca si vous tenez à votre matériel et vos oreilles
+    if(flag==0){
+        if(oct==3){
+            return(oct); //sécurité pour eviter les fréquence trop basses
+        }
+        oct--;
+    }else{
+        if(oct==5){
+            return(oct); //sécurité pour eviter les fréquence trop hautes
+        }
+        oct++;
+    }
+}
+
 
 int main (void)
 {
--- tune_sox.c
+++ tune_sox.c
@@ -5,17 +5,35 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define Harm 15 // number of harmonics to play, feel free to mess with that
+
+void tune(int);
+void octSwap(int);
+
+//int Freq3[12] ={131,139,147,156,165,175,185,196,207,220,233,247}; à titre indicatif
+int Freq[12] ={262,277,293,311,330,349,370,392,415,440,466,493};
+//int Freq5[12] ={523,554,587,622,659,698,740,784,831,880,932,988}; à titre indicatif
+
+int oct=4;
+
 int main () {
-    int freq;
-    
+
+    octSwap(0);
+    tune(Freq[0]);
+}
+
+void tune(int freq){ //make sure that sox is installed before running
+//the way this function works is that it create a string which contains the sox command and then use it via the system() call
+
+  
     pid_t  pid;
+    if(oct==5){freq=freq*2;}
+    if(oct==3){freq=freq*0.5;}
 
-    freq=440;
-    int volu=-10;
+    int volu=-10; // change that at your own risk, may harm your speaker and/or your hears
 
-    for(int i;i<10;i++){
+    for(int i;i<Harm;i++){
         char command[100];
-        
         
         strcpy(command,"play -n -c1 synth 1 sine "); // sox command
         char buffer[50];
@@ -28,15 +46,33 @@ int main () {
         strcat(command,"db");
         printf("\n%s\n",command);
         pid=fork();
-        if(pid==0){
+        if(pid==0){ //to play all the harmonics at the same time we need to do some kind of multi threading
             system(command);
+            sleep(1);
             exit(-1);
         }
-        freq=freq*2;
-        volu=volu-10;
+        freq=freq*2; //maybe we can get some special sounds by messing with that
+        volu=volu-10; //here you can mess with the harmonic spectrum
     }
     
-    for(int i;i<10;i++){
+    for(int i;i<Harm;i++){
+        
         wait(0);
+    }
+
+}
+
+void octSwap(int flag){ //le flag 0 indique que l'on veut descendre d'un octave / le flag 1 indique que l'on veut monter d'un octave
+  //WARNING techniquement on peut utiliser cette commande une infinité de fois pour avoir des fréquence tres basses ou tres haute ... ne faites pas ca si vous tenez à votre matériel et vos oreilles
+    if(flag==0){
+        if(oct==3){
+            return(oct); //sécurité pour eviter les fréquence trop basses
+        }
+        oct--;
+    }else{
+        if(oct==5){
+            return(oct); //sécurité pour eviter les fréquence trop hautes
+        }
+        oct++;
     }
 }
