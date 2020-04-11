b54978d8502e09b8ea4b4679af15aaa958a5ca9c
Guigeek <geegeek21@gmail.com>
2020-04-11 18:21:15 +0200

    New feature ! you can now switch between 2 Harmonic mode
    also sorted things

--- SynthPi.c
+++ SynthPi.c
@@ -1,29 +1,55 @@
+
+// SynthPi Work in Progress
+
+//make sure that sox is installed before running, see readme.md
+
+
+//========================Lib==========================================
 #include <stdio.h>
+#include <wiringPi.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+//=====================================================================
 
-#define Harm 10 // number of harmonics to play, feel free to mess with that
-
+//================================Prototype============================
 void tune(int);
 int octSwap(int);
+int harmSwap();
+void scanButton(int);
+void function(int);
+//=====================================================================
 
-//int Freq3[12] ={131,139,147,156,165,175,185,196,207,220,233,247}; à titre indicatif
+//==============Valeur par défaut (inutile de les modifier)============
 int Freq[12] ={262,277,293,311,330,349,370,392,415,440,466,493};
-//int Freq5[12] ={523,554,587,622,659,698,740,784,831,880,932,988}; à titre indicatif
-int globalVol=-5; // in dB, change that at your own risk, may harm your speaker and/or your hears
-
-int oct=4;
+//les freq correspondent à l'octave 4 (définie vulgairement comme le centre du piano) voici les notes dans l'ordre :
+//Do4 (C4), Do#4 (C#4), Ré4 (D4), Ré#4 (D#4), Mi4 (E4), Fa4 (F4), Fa#4 (F#4), Sol4 (G4), Sol#4 (G#4), La4 (A4), La#4 (A#4), Si4 (B4)
 int HarmMode=1;
+int oct=4;
+//====================================================================
 
-int main () {
-    
-    tune(Freq[0]);
+int globalVol=-5; // in dB, change that at your own risk, may harm your speaker and/or your hears
+int Harm=10;
+
+void scanButton (int boutton) 
+{
+  if (digitalRead (boutton) == HIGH)	// le bouton est pressé
+    return ;
+
+  fonction(boutton);
+
+  while (digitalRead (boutton) == LOW)	// on attend que le bouton soit relaché
+    delay (10) ;
 }
 
-void tune(int freq){ //make sure that sox is installed before running
+void fonction(int boutton)
+{
+  printf("boutton" + boutton);
+}
+
+void tune(int freq){ 
 //the way this function works is that it create a string which contains the sox command and then use it via the system() call
 
   
@@ -84,7 +110,7 @@ int octSwap(int flag){ //le flag 0 indique que l'on veut descendre d'un octave /
     }
 }
 
-int HarmSwap(){
+int harmSwap(){
     if(HarmMode){
         HarmMode=0;
         return(HarmMode);
@@ -92,4 +118,30 @@ int HarmSwap(){
         HarmMode=1;
         return(HarmMode);
     }
+}
+
+
+int main (void)
+{
+  int i, a ; //a servira à savoir si le boutton d'arret a été pressé ou non
+
+    tune(Freq[6]);
+
+  wiringPiSetup () ;
+
+// Setup des inputs
+
+  for (i = 0 ; i < 20 ; ++i)
+  {
+    pinMode         (i, INPUT) ;
+    pullUpDnControl (i, PUD_UP) ;
+    //boutton [i] = 0 ;
+  }
+
+  while (a!=0)
+  {
+    for (i = 0 ; i < 20 ; ++i)
+      scanButton (i) ;
+    delay (1) ;
+  }
 }
